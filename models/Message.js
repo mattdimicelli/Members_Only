18ed9d69c0c2fdfe0da81112c5dc0f6424331035
Matt Di Micelli <matthewdimicelli@gmail.com>
2022-04-02 16:27:26 -0400

    build models

---
+++ models/Message.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require('mongoose');
+
+const messageSchema = new Schema({
+    title: { 
+        type: String, 
+        required: true, 
+        trim: true, 
+        minLength: 1,
+    },
+    timestamp: { 
+        type: Date, 
+        required: true, 
+        default: Date.now,
+    },
+    body: { type: String, required: true, trim: true, minLength: 1},
+    author: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
+});
+
+module.exports = model('Message', messageSchema);
---
+++ models/User.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+const { isAlpha, isEmail } = require('validator');
+
+const alphaValidator = [input => isAlpha(input, {ignore: ' '}),
+                         '{PATH} must be letters and space only' ];
+
+const emailValidator = [input => isEmail(input), '{PATH} must be an email address'];
+
+
+const userSchema = new Schema({
+    firstName: { 
+        type: String, 
+        required: true, 
+        trim: true, 
+        validate: alphaValidator,
+        minLength: 1,
+    },
+    lastName: { 
+        type: String, 
+        required: true, 
+        trim: true, 
+        validate: alphaValidator,
+        minLength: 1
+    },
+    email: { type: String, required: true, trim: true, validate: emailValidator },
+    member: { type: Boolean, required: true },
+});
+
+userSchema.virtual('fullName')
+    .get(function() {
+        return `${this.firstName} ${this.lastName}`;
+    })
+    .set(function())
+
+module.exports = model('User', userSchema);
