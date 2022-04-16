634a714bd2d111385d3303214588bd5e3bf24ad7
Matthew Di Micelli <matthewdimicelli@gmail.com>
2022-04-16 16:36:04 -0400

    add custom daisyui color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,22 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        nintendo: {
+         primary: "#d2c514",
+         secondary: "#4f3c89",
+         accent: "#c23f3d",
+         neutral: "#211f20",
+         "base-100": "#fefee6",
+         info: "#CAE2E8",
+         success: "#DFF2A1",
+         warning: "#F7E488",
+          error: "#F2B6B5",
+        },
+      },
+    ],
+  },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
 }
