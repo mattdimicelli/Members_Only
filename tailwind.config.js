af524d8c2ce7f7eb037a4e98976ee63fe18f68e8
Matthew Di Micelli <matthewdimicelli@gmail.com>
2022-04-16 17:25:23 -0400

    add logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,9 @@ module.exports = {
     themes: [
       {
         nintendo: {
-         primary: "#d2c514",
+         primary: "#fef906",
          secondary: "#4f3c89",
-         accent: "#c23f3d",
+         accent: "#f4290f",
          neutral: "#211f20",
          "base-100": "#fefee6",
          info: "#CAE2E8",
