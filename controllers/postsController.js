eaf2e68600f01d85665f60556faf3f59c99e6223
Matthew Di Micelli <matthewdimicelli@gmail.com>
2022-04-18 19:25:40 -0400

    make create post page

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,5 +1,29 @@
 const Post = require('../models/Post');
 
+exports.createPostGet = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        res.render('create_post', {title: 'Post a Message'});
+    }
+    else {
+        req.flash('login_status', 'Not logged in');
+        res.redirect('/');
+    }
+}
+
+exports.createPostPost = async (req, res, next) => {
+    try {
+        const { message: body, title } = req.body;
+        const author = req.user;
+        const post = new Post({title, body, author});
+        await post.save();
+        res.redirect('/');
+    }
+    catch(err) {
+        next(err);
+    }
+    
+}
+
 // Display home page (posts)
 exports.homeGet = async (req, res, next) => {
     const errorsObj = req.session.errorsObj;
@@ -7,7 +31,8 @@ exports.homeGet = async (req, res, next) => {
 
     try {
         const posts = await Post.find({}).exec();
-        res.render('posts', { posts, title: 'Home', errors: errorsObj });
+        res.render('posts', { posts, title: 'Home', errors: errorsObj, 
+            message: req.flash('login_status') });
     }
     catch(err) {
         next(err);
--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { homeGet } = require('../controllers/postsController.js');
+const { homeGet, createPostGet, createPostPost } = require('../controllers/postsController.js');
 const router = express.Router();
 
 /* GET req to READ Posts home page */
 router.get('/', homeGet);
+router.get('/create-post', createPostGet);
+router.post('/create-post', createPostPost);
 
 module.exports = router;
